9ab3a3b22f7b54bcb2b18305d7cc89ed93cc22c6
Francisco Granja <franbbr110@gmail.com>
2021-07-24 01:01:48 -0300

    Final

--- netlify/functions/next.js
+++ netlify/functions/next.js
@@ -1,18 +1,7 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event, context, callback) => {
-  // const url = event.headers.referer;
-  // //console.log(url);
-
-  // const qs = new URL(url);
-  // //console.log(qs);
-
-  // const pathname = qs.pathname;
-  // // const term = qs.searchParams.get('term');
   let location = event.body;
-  //   let offset = event.body;
-  //console.log(pathname);
-  //console.log(term);
   location = location.split('"');
   let offset = location[6].substring(1).slice(0, -1);
   offset = parseInt(offset);
@@ -26,11 +15,8 @@ exports.handler = async (event, context, callback) => {
 
   const YELP = "https://api.yelp.com/v3/businesses";
   const ENDPOINT = `${YELP}/search?term=parking&location=${location[3]}&sort_by=rating&offset=${next}`;
-  //   console.log(ENDPOINT);
 
-  //const API_KEY = '';
-  const API_KEY =
-    "mi5qSSqdhmrNXBjLq5MBMwuqcS0q8aE4u52fwqrG8CkrBjjksgdV8ZblHdh4ThtDqQVFapfOwrCqadcTH4sJIMhQgEcWpc0bK_9ms_rJ1H-xMT1Amp4tmH_PhAg3X3Yx";
+  const API_KEY = process.env.YELP_API_KEY;
 
   const raw_data = await fetch(ENDPOINT, {
     headers: {
@@ -39,13 +25,10 @@ exports.handler = async (event, context, callback) => {
       withCredentials: true,
     },
   });
-  // console.log(event);
 
   let data = await raw_data.json();
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(data),
   });
-  //   console.log(data);
 };
-// © 2021 GitHub, Inc.
