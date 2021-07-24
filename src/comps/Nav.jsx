11d5dda5838cbaae539f67582aa58d4b9cfeae05
Francisco Granja <franbbr110@gmail.com>
2021-07-23 21:41:49 -0300

    Final

--- src/comps/Nav.jsx
+++ src/comps/Nav.jsx
@@ -1,30 +1,30 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
-const Nav = ({
-    setSubmitting, setEntering, setFetchedVisible
-}) => {
+// const Nav = ({
+//     setSubmitting, setEntering, setFetchedVisible
+// }) => {
 
-    const cleanInputsFx = (e) => {
-        e.preventDefault();
-        setFetchedVisible(false);
-        setEntering({
-            food: '',
-            town: ''
-        });
-        setSubmitting({
-            food: '',
-            town: ''
-        });
-    };
+//     const cleanInputsFx = (e) => {
+//         e.preventDefault();
+//         setFetchedVisible(false);
+//         setEntering({
+//             food: '',
+//             town: ''
+//         });
+//         setSubmitting({
+//             food: '',
+//             town: ''
+//         });
+//     };
 
-    return (
-        <div className="Nav">
-            {/* <p>this is FxComp `Nav.jsx`</p> */}
-            <ul>
-                <li onClick={cleanInputsFx}><NavLink to="/">homepage</NavLink></li>
-            </ul>
-        </div>
-    )
-};
+//     return (
+//         <div className="Nav">
+//             {/* <p>this is FxComp `Nav.jsx`</p> */}
+//             <ul>
+//                 <li onClick={cleanInputsFx}><NavLink to="/">homepage</NavLink></li>
+//             </ul>
+//         </div>
+//     )
+// };
 
-export default Nav;
+// export default Nav;
